e434ac43e142fcb9d0bba1e445aeffe894575c81
Daniel Maina <dan@Dan-2.local>
2018-04-11 15:08:56 +0300

    test(stack): add stack tests

--- test/stack.test.js
+++ test/stack.test.js
@@ -6,6 +6,10 @@ var stackFixture = new Stack()
 stackFixture.storage = "one-two-three-four";
 
 describe('Stack', function () {
+	beforeEach(function() {
+		stackFixture.storage = "one-two-three-four";
+	})
+	
 	it('should reject pushing empty values to stack', function () {
 		var stack = new Stack()
 		assert.equal(stack.push(''), 'Cannot push an empty value to stack')
@@ -27,7 +31,7 @@ describe('Stack', function () {
 
 	it('should pop elements from the stack', function () {
 		stackFixture.pop()
-		assert.equal(stackFixture.size(), 5)
+		assert.equal(stackFixture.size(), 3)
 	})
 
 	it('should reject pop elements from empty stack', function () {
@@ -42,11 +46,11 @@ describe('Stack', function () {
 		chai.expect(stack.head()).to.equals('The stack is empty.')
 	})
 
-	// it('should get tail element from stack', function () {
-	// 	chai.expect(stackFixture.tail()).to.equals('five')
-	// 	// empty stack
-	// 	var stack = new Stack()
-	// 	chai.expect(stack.tail()).to.equals('The stack is empty.')
-	// })
+	it('should get tail element from stack', function () {
+		chai.expect(stackFixture.tail()).to.equals('four')
+		// empty stack
+		var stack = new Stack()
+		chai.expect(stack.tail()).to.equals('The stack is empty.')
+	})
 	
 })
