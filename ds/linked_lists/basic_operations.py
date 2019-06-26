6a07aff88e813a46b1e3cb247912a4a6ec58105c
danielwangai <mainad57@gmail.com>
2019-06-26 18:35:44 +0300

    get linked list size iteratively and recursively

--- ds/linked_lists/basic_operations.py
+++ ds/linked_lists/basic_operations.py
@@ -128,33 +128,53 @@ class LinkedList:
         self.head = self.head.next
         return self.delete_all_nodes_recursive()
 
+    def get_size_iteratively(self):
+        if not self.head:
+            return 0
+        count = 0
+        while(self.head):
+            if not self.head.next:
+                return count
+            self.head = self.head.next
+            count += 1
+            continue
+        return count
+
+    def get_size_recursively(self):
+        if not self.head:
+            return 0
+        self.head = self.head.next
+        count = 1 + self.get_size_recursively()
+        return count
+
+
 node = Node(3)
 node2 = Node(4)
 node.next = node2
 l = LinkedList()
 l.head = node
 # insert 6 at tail
-l.insert_tail(6)
-print(l.head.next.next.data)
+# l.insert_tail(6)
+# print(l.head.next.next.data)
 
-l.insert_tail(7)
-print("last ", l.head.next.next.next.data)
+# l.insert_tail(7)
+# print("last ", l.head.next.next.next.data)
 
-# insert 5 after node 4
-l.insert_after(4, 5)
-print(l.head.next.next.next.data)
+# # insert 5 after node 4
+# l.insert_after(4, 5)
+# print(l.head.next.next.next.data)
 
 # before deleting 4
-print(l.head.next.data)
-# delete node 4
-l.delete_node(7)
-print(l.head.next.data)
+# print(l.head.next.data)
+# # delete node 4
+# l.delete_node(7)
+# print(l.head.next.data)
 
 # before deleting n = 2
-print("before delete nth: ", l.head.next.next.data)
-# delete the 3rd node; n = 2
-l.delete_nth_node(2)
-print("after delete nth: ", l.head.next.next.data)
+# print("before delete nth: ", l.head.next.next.data)
+# # delete the 3rd node; n = 2
+# l.delete_nth_node(2)
+# print("after delete nth: ", l.head.next.next.data)
 
 # print("Before delete all iterative: ", l.head.data)
 # l.delete_all_nodes_iterative()
@@ -163,3 +183,7 @@ print("after delete nth: ", l.head.next.next.data)
 # print("Before delete all recursive: ", l.head.data)
 # l.delete_all_nodes_recursive()
 # print("After delete all recursive: ", l.head)
+l.insert_tail(8)
+l.insert_tail(9)
+print("Size iteratively: ", l.get_size_iteratively())
+print("Size recursively>>: ", l.get_size_recursively())
