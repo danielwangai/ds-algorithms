f6a925ace73867af1f696e0c43367c5ffb351613
danielwangai <mainad57@gmail.com>
2019-07-02 13:41:49 +0300

    bst insert iterative and recursive

--- ds/trees/tree.py
+++ ds/trees/tree.py
@@ -1,12 +1,13 @@
-class Tree(object):
+class Node(object):
     # structure of a node
     # optional parameter
+    nodes = []
     def __init__(self, data = None):
         self.left = None
         self.right = None
         self.data = data
 
-    def insert(self, number):
+    def insert_recursive(self, number):
         """
         [x] check if root is empty
             - [x] if empty assign number to root
@@ -20,23 +21,61 @@ class Tree(object):
             - check if right is empty; if empty
                 - [x] initialize a new node here and set it's value to number
             - [x] else(right node has a value) recurse the function.
-
+        complexity: O(log(n))
         """
         # check if a root is empty
-        if self.data == None:
+        if not self:
             # set value to root
+            root = Node(number)
+            return
+        else:
+            if number < self.data:
+                # to the left
+                if self.left is None:
+                    self.left = Node(number)
+                else:
+                    self.left.insert_recursive(number)
+            elif number > self.data:
+                # to the right
+                if self.right is None:
+                    self.right = Node(number)
+                else:
+                    self.right.insert_recursive(number)
+
+    def insert_iterative(self, number):
+        if not self:
             self.data = number
-            # return
-        if number <= self.data:
-            # to the left
-            if self.left is None:
-                self.left = Tree(number)
-            else:
-                self.left.insert(number)
-        elif number >= self.data:
-            # to the right
-            if self.right is None:
-                self.right = Tree(number)
-            else:
-                self.right.insert(number)
-        print(">>>> ", self.data)
+            return self
+        else:
+            while(self):
+                if number > self.data:
+                    if self.right:
+                        self = self.right
+                    else:
+                        self.right = Node(number)
+                        break
+                if number < self.data:
+                    if self.left:
+                        self = self.left
+                    else:
+                        self.left = Node(number)
+                        break
+
+
+node = Node(4)
+# insert(node, 4)
+"""
+node.insert_recursive(5)
+node.insert_recursive(3)
+node.insert_recursive(2)
+node.insert_recursive(1)
+print(node.left.left.data)
+"""
+
+"""
+node.insert_iterative(5)
+node.insert_iterative(3)
+node.insert_iterative(2)
+node.insert_iterative(1)
+print(node.left.left.data)
+"""
