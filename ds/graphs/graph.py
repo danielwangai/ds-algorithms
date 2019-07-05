550fa0c2061d609b048f76e60999f926bcd1ade0
danielwangai <mainad57@gmail.com>
2019-07-06 00:18:17 +0300

    implement graph data structure
    - add vertex to graph
    - add undirected edge between vertices
    - add directed edge between vertices

---
+++ ds/graphs/graph.py
@@ -0,0 +1,53 @@
+class Graph:
+    """
+    graph implementation using adjacency list
+    format:
+        {
+            "vertex": [list of adjacent vertices]
+        }
+    """
+    def __init__(self):
+        self.adjacency_list = {}
+    
+    def add_vertex(self, vertex):
+        if vertex in self.adjacency_list.keys():
+            return None
+        self.adjacency_list[vertex] = []
+
+    def add_edge_undirected(self, vertex_a, vertex_b):
+        """
+        create an undirected edge/path between vertices a and b
+        """
+        if vertex_a not in self.adjacency_list.keys() or not vertex_b in self.adjacency_list.keys():
+            # both vertices must exist to create an edge to join them
+            return None
+        # only add an edge if there's no edge joining them.
+        if vertex_a not in self.adjacency_list[vertex_b]:
+            self.adjacency_list[vertex_b].append(vertex_a)
+        if vertex_b not in self.adjacency_list[vertex_a]:
+            self.adjacency_list[vertex_a].append(vertex_b)
+    
+    def add_edge_directed(self, vertex_a, vertex_b):
+        """
+        create an edge from vertex a to b ONLY
+        """
+        if vertex_a not in self.adjacency_list.keys() or vertex_b not in self.adjacency_list.keys():
+            # both vertices must exist to create an edge to join them
+            return None
+        # print("vertex b: ", vertex_b, vertex_a)
+        if vertex_b not in self.adjacency_list[vertex_a]:
+            self.adjacency_list[vertex_a].append(vertex_b)
+
+"""
+# uncomment to run
+
+graph = Graph()
+graph.add_vertex("Nairobi")
+graph.add_vertex("Mombasa")
+print(graph.adjacency_list)
+graph.add_edge_undirected("Nairobi", "Mombasa")
+print(graph.adjacency_list)
+graph.add_vertex("Kisumu")
+graph.add_edge_directed("Kisumu", "Mombasa")
+print(graph.adjacency_list)
+"""
